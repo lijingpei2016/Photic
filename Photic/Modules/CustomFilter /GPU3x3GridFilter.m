58b73c4fe29d1024320179cf460ac0cdea210dce
李境沛 <as330732842@qq.com>
2023-07-19 11:59:29 +0800

    Modify:更新3分滤镜的纹理坐标写法
    Modify:更新3分滤镜的纹理坐标写法

--- Photic/Modules/CustomFilter /GPU3x3GridFilter.m
+++ Photic/Modules/CustomFilter /GPU3x3GridFilter.m
@@ -18,21 +18,21 @@ NSString *const kGPUImage3x3GridFragmentShaderString = SHADER_STRING
     highp vec2 textureCoord = textureCoordinate;
 
     //横向坐标分成三块
-    if (textureCoord.s <= 0.333) {
-        textureCoord.s *= 3.0;
+    if (textureCoord.x <= 0.333) {
+        textureCoord.x *= 3.0;
     } else if (textureCoord.s <= 0.666) {
-        textureCoord.s = (textureCoord.s - 0.333) * 3.0;
+        textureCoord.x = (textureCoord.s - 0.333) * 3.0;
     } else {
-        textureCoord.s = (textureCoord.s - 0.666) * 3.0;
+        textureCoord.x = (textureCoord.s - 0.666) * 3.0;
     }
 
     //纵向坐标分成三块
-    if (textureCoord.t <= 0.333) {
-        textureCoord.t *= 3.0;
+    if (textureCoord.y <= 0.333) {
+        textureCoord.y *= 3.0;
     } else if (textureCoord.t <= 0.666) {
-        textureCoord.t = (textureCoord.t - 0.333) * 3.0;
+        textureCoord.y = (textureCoord.t - 0.333) * 3.0;
     } else {
-        textureCoord.t = (textureCoord.t - 0.666) * 3.0;
+        textureCoord.y = (textureCoord.t - 0.666) * 3.0;
     }
     gl_FragColor = texture2D(inputImageTexture, textureCoord);
 }
